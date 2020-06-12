f883455871803d544d8322ad4f085bfd15d47f23
jkim0715 <gjr191@naver.com>
2020-06-12 14:28:03 +0900

    Movie Serializers | Movie , MovieList

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     'accounts',
     'reviews',
     'django_extensions',
+    #cors
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
-import requests
+from django.shortcuts import render,get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import MovieSerializer,MovieListSerializer
+from .models import Movie
 # Create your views here.
 
+#1. API화면 -> api_view
+#2. 사용자에게 응답을 해주는 도구 -> Response
+@api_view(['GET'])
 def index(request):
-    url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=1144cdf8e1a2dab59dbca2112342e131&targetDt=20120101'
-    print(requests)
-    response = requests.get(url).json()
-    return ''
+    movies = Movie.objects.all()
+    serializer = MovieListSerializer(movies, many=True)
+    return Response(serializer.data)
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create(request):
+    serializer = MovieSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user = request.user) # NOT NULL CONSTRAINT FAILED (ID가 없을 때)
+        return Response(serializer.data)
+    return ''
+# 글 쓰는 사람 -> 장고 바깥 (Vue CLI)
+# 글 쓰는 사람이, 글을 쓸 때 '내가 누구다!' 라는 정보를 함께 보낼 것. 'geunje', 'pwd123'
+# Token이라는 것을 만들어서 보낼 것임.
+#
+
+@api_view(['GET'])
+def detail(request,article_id):
+    movie = get_object_or_404(Movie, pk=article_id)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
