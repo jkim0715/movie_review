5e8ee1e413c5e6f2200fbbdf0437e28fa6bbb9db
jkim0715 <gjr191@naver.com>
2020-06-15 03:32:33 +0900

    MovieSearch by title

--- movies/urls.py
+++ movies/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 
   path('', views.index),
-  path('search/<str:movie_title>/', views.search), #영화 이름을 찾기
+  path('searchmovie/<str:movie_title>/', views.search), #영화 이름을 찾기
   path('detail/<int:movie_id>/', views.detail), #영화 디테일 
   path('moviecomment/<str:movie_title>/', views.moviecomment), #영화 한줄평 리스트
   path('moviecomment/<int:movie_id>/create', views.createmoviecomment), #영화 한줄평 작성
