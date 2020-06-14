2c805599ed2894467d3a37f67222b7759adcf8f8
jkim0715 <gjr191@naver.com>
2020-06-14 16:24:46 +0900

    Genre List | Review Comment List

--- movies/urls.py
+++ movies/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
 
   path('', views.index),
-  path('<str:movie_title>/', views.search),
-  path('detail/<int:movie_id>/', views.detail),
-  path('moviecomment/<str:movie_title>/', views.moviecomment),
-  path('moviecomment/<int:movie_id>/create', views.createmoviecomment),
-  path('genre/',views.findgenre),
+  path('search/<str:movie_title>/', views.search), #영화 이름을 찾기
+  path('detail/<int:movie_id>/', views.detail), #영화 디테일 
+  path('moviecomment/<str:movie_title>/', views.moviecomment), #영화 한줄평 리스트
+  path('moviecomment/<int:movie_id>/create', views.createmoviecomment), #영화 한줄평 작성
+  path('genre/',views.findgenre), #모든 영화장르 
 ]
